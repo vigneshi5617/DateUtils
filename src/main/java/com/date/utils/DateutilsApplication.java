5bb7827a72afabe45bc9518523ec6866be4e6171
Vignesh Anand <vigneshi5617@gmail.com>
2022-04-21 13:48:39 +0530

    Complted the DateUtils and tested it

--- src/main/java/com/date/utils/DateutilsApplication.java
+++ src/main/java/com/date/utils/DateutilsApplication.java
@@ -1,5 +1,6 @@
 package com.date.utils;
 
+import java.util.Date;
 import java.util.Scanner;
 
 import org.slf4j.Logger;
@@ -17,10 +18,36 @@ public class DateutilsApplication {
 		
 		String date;
 		date =new String("12/23/2021");
-		log.info("date is ->{}",DateUtils.formatDDMMYYYYfromDate(date));
+		log.info("date is ->{}",DateUtils.formatDDMMYYYYFromDate(date));
 		date=new String("2021-11-29");
 		
 		log.info("date is ->{}",DateUtils.formatYYYYMMDDStringToMMDDYYYYString(date));
+	
+		date = new String("04/09/2022");
+		
+		log.info("date is ->{}",DateUtils.formatStringYYYYMMDDTOMMDDYYYYDate(date));
+		
+		
+		Date d = new Date();
+		log.info("the date is ->{}",d);
+		log.info("date is ->{}",DateUtils.formatDateWithCDTTimestamp(d));
+		
+		
+		//Very useful method down please use this 
+		log.info("date is ->{}",DateUtils.formatDateAsMMDDYYYY(d));
+		
+		
+		
+		log.info("date is ->{}",DateUtils.formatDateAsTimeStamp(d));
+		
+		
+		date =new String("03/06/2007 13:44 PM");
+		
+		log.info("date is ->{}",DateUtils.formatMMDDYYYhhmmaaFromDateTimeStr(date));
+		
+		
+		
+		
 		
 	}
 
